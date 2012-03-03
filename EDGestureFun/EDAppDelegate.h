a31f7bbe64fb3fc5b7d4a03124a7c83880674405
Evan Davis <Evan@Evan-Daviss-iMac.local>
2012-03-03 15:03:39 -0600

    Initial Commit

---
+++ EDGestureFun/EDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EDAppDelegate.h
+//  EDGestureFun
+//
+//  Created by Evan Davis on 3/3/12.
+//  Copyright (c) 2012 WillowTree Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EDGestureFun/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EDGestureFun
+//
+//  Created by Evan Davis on 3/3/12.
+//  Copyright (c) 2012 WillowTree Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EDAppDelegate class]));
+    }
+}
